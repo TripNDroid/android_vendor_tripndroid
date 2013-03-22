db2571af39c525612b1e1f6c1c2fd628db5c3e18
TripNRaVeR <noerialexeew@live.nl>
2013-03-23 00:30:35 +0100

    update default ringtone

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -3,7 +3,7 @@ $(call inherit-product, vendor/tripndroid/config/common_full.mk)
 
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Orion.ogg \
+    ro.config.ringtone=QuietlyBrilliant.ogg \
     ro.config.notification_sound=Gamma.ogg \
     ro.config.alarm_alert=CyanAlarm.ogg
 
