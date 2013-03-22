cbae7b51c5f2ddcbd42fb7db8e6992dcfe05a47b
TripNRaVeR <noerialexeew@live.nl>
2013-03-23 00:28:01 +0100

    update default audio tones

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -4,8 +4,8 @@ $(call inherit-product, vendor/tripndroid/config/common_full.mk)
 # Default ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Orion.ogg \
-    ro.config.notification_sound=Deneb.ogg \
-    ro.config.alarm_alert=Hassium.ogg
+    ro.config.notification_sound=Gamma.ogg \
+    ro.config.alarm_alert=CyanAlarm.ogg
 
 PRODUCT_PACKAGES += \
   Mms
