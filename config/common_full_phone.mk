fff50a510160e3bc44b0b2fe190b41ca2768769c
TripNRaVeR <noerialexeew@live.nl>
2013-08-17 00:34:29 +0200

    remove bluetooth config (cm)

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -9,7 +9,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 PRODUCT_PACKAGES += \
   Mms
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.conf:system/etc/bluetooth/main.conf
--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -6,7 +6,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Orion.ogg \
     ro.config.notification_sound=Deneb.ogg \
     ro.config.alarm_alert=Hassium.ogg
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
